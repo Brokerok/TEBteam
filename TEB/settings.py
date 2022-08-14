947f7a77919165e3c851c7d4639a5165e5854d29
Pavlo Shev4uk <95368807+Brokerok@users.noreply.github.com>
2022-08-14 19:16:44 +0300

    Update settings.py

--- TEB/settings.py
+++ TEB/settings.py
@@ -1,17 +1,6 @@
 import os
-
-# import environ
 from django.urls import reverse_lazy
-#
-# env = environ.Env(DEBUG=(bool, False))
-# root = environ.Path(__file__) - 2
-# environ.Env.read_env()
-#
-# BASE_DIR = str(root)
-# SECRET_KEY = env.str('SECRET_KEY', '!!! SET YOUR SECRET_KEY !!!')
-#
-# DEBUG = env.bool('DEBUG', True)
-# ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['*'])
+
 try:
     from .local_settings import *
 except ImportError:
